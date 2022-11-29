e6f7b791eec67b491c29643e4f8d4c2026f2104c
Santiago Carranza <santiago.c.trjo@gmail.com>
2022-11-29 15:39:18 -0300

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.spoonacular.com/',
   apiKey1: '9fa6fc05c627466f877f456e296c938d',
-  apiKey2:'42d35eacba4b4485a1e8820186f8cec6'
+  apiKey2:'42d35eacba4b4485a1e8820186f8cec6',
+  firebase:'https://a-la-carta-alkemy-default-rtdb.firebaseio.com/datos.json'
+
 };
